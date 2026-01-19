12e0bb8d2718ded6f1c0355f6f93c6a611ee38b7
Diego Villagrán <d.villagranf@duocuc.cl>
2026-01-19 16:57:25 -0300

    feat: se terminan las entidades faltantes

---
+++ src/main/java/com/springboot_sa_ha1/modules/collections/model/Collection.java
@@ -0,0 +1,27 @@
+package com.springboot_sa_ha1.modules.collections.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "colecciones")
+@Getter
+@Setter
+
+public class Collection {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id_collection;
+
+  @NotBlank
+  private String collection_name;
+
+  private String description;
+
+
+
+
+}
