b434bc8a3ef1c9063e2a37273dc0da9058e0195b
Tiago Alcázar <tiagofabian195@outlook.com>
2026-01-21 15:20:38 -0300

    enable cors

--- src/main/java/com/springboot_sa_ha1/security/config/CorsConfig.java
+++ src/main/java/com/springboot_sa_ha1/security/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
     CorsConfiguration config = new CorsConfiguration();
 
     config.setAllowedOrigins(List.of(
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://hoseki.vercel.app"
     ));
 
     config.setAllowedMethods(List.of(
